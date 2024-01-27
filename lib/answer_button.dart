40dbf16cfd5d2b3e3d09714ede0ff5c380ef6a36
Veng-Eang <oengvengeang@gmail.com>
2024-01-26 23:58:51 -0500

    add new

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -20,7 +20,10 @@ class AnswerButton extends StatelessWidget {
         ),
       ),
       onPressed: onTap,
-      child: Text(answerText),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,7 @@
 import 'package:adv_basic/answer_button.dart';
+import 'package:adv_basic/data/questions.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,36 +13,45 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
+    final currentQuestion = questions[currentQuestionIndex];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            "The Questions...",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
+      child: Container(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: GoogleFonts.lato(
+                fontSize: 28,
+                color: const Color.fromARGB(255, 201, 153, 251),
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          AnswerButton(
-            answerText: "answer 1",
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: "answer 2",
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: "answer 3",
-            onTap: () {},
-          ),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: answerQuestion,
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
